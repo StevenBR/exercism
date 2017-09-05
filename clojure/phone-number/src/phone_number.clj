67dd05993374b21d2c9e17f18df61de325b59e02
Steven Rodriguez <stevenbrodriguez@gmail.com>
2017-09-05 14:15:59 -0700

    refactored to parse US numbers in it's own function

--- clojure/phone-number/src/phone_number.clj
+++ clojure/phone-number/src/phone_number.clj
@@ -1,27 +1,30 @@
 (ns phone-number)
 
-(defn parse-number
+(defn- parse-number
   [number]
   (->> number
        (re-seq #"[0-9]+")
        (apply str)))
 
+(defn parse-us-number
+  [n]
+  (if (and (= (count n) 11) (= (first n) \1))
+                (drop 1 n)
+                (repeat 10 "0")))
+
 (defn number
   [num]
   (let [n (parse-number num)]
     (->> (if (= 10 (count n))
-        n
-        (if (and (= (count n) 11) (= (first n) \1))
-            (drop 1 n)
-            (repeat 10 "0")))
+            n
+            (parse-us-number n))
         (apply str))))
 
 (defn area-code
   [num]
-  (->> num
+  (-> num
        number
-       (take 3)
-       (apply str)))
+       (subs 0 3)))
 
 (defn pretty-print
   [num]

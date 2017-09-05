a23723e74bc9fa86266e6ceceec5d37b0084dd54
Steven Rodriguez <stevenbrodriguez@gmail.com>
2017-09-05 14:04:02 -0700

    everything works

--- clojure/phone-number/src/phone_number.clj
+++ clojure/phone-number/src/phone_number.clj
@@ -6,13 +6,6 @@
        (re-seq #"[0-9]+")
        (apply str)))
 
-(defn something
-  []
-  (->> "1(503)602-0442"
-      parse-number
-      (as-> num
-            num)))
-
 (defn number
   [num]
   (let [n (parse-number num)]
@@ -23,23 +16,6 @@
             (repeat 10 "0")))
         (apply str))))
 
-(something)
-(count "11234567890")
-(drop 1 (parse-number "11234567890"))
-(drop 1 (parse-number "1(503)602-0442"))
-(drop 1 (parse-number "(503)602-0442"))
-(if (= 10 (count (parse-number "(503)602-0442")))
-  (print "this was true ********")
-(if (and (= 11 (count (parse-number "1(503)602-0442"))) (= \1 (first (parse-number "1(503)602-0442"))))
-  (drop 1 (something))
-  (print "you*****")))
-(number "1(503)602-0442")
-(number "(503)602-0442")
-
-(defn size
-  [num]
-  (count num))
-
 (defn area-code
   [num]
   (->> num
@@ -47,8 +23,8 @@
        (take 3)
        (apply str)))
 
-(area-code "11234567890")
-
 (defn pretty-print
-  [])
+  [num]
+  (let [n (number num)]
+      (format "(%s) %s-%s" (subs n 0 3) (subs n 3 6) (subs n 6 10))))
 
